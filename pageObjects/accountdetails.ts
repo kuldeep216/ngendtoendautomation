0fa95b7adaff2e7cfb32f94b70e5d0b452f2a421
kuldeep216 <55515430+kuldeep216@users.noreply.github.com>
2021-01-27 03:02:41 +0530

    Create accountdetails.ts

---
+++ pageObjects/accountdetails.ts
@@ -0,0 +1,16 @@
+import { isThisTypeNode } from "typescript";
+
+export class AccountDetails
+{
+    kdEmail: string;
+    kdPass: string;
+    kdName: string;
+
+    constructor()
+    {
+        this.kdEmail = "kdstunewdb@gmail.com";
+        this.kdPass = "summer2020";
+        this.kdName = "Kuldeep";
+    }
+
+}
